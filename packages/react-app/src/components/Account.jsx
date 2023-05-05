fc550e7fa9a1540aec00b4d99c8cc3c8fb592693
Vlad Shlapakov <sshlapakovvlad@googlemail.com>
2023-05-05 12:12:18 +0300

    add react moralis and web3uikit, change connect wallet button

--- packages/react-app/src/components/Account.jsx
+++ packages/react-app/src/components/Account.jsx
@@ -1,45 +1,46 @@
 import { Button } from "antd"
 import React from "react"
 import { useThemeSwitcher } from "react-css-theme-switcher"
+import { ConnectButton } from "@web3uikit/web3"
 
 import Address from "./Address"
 import Balance from "./Balance"
 import Wallet from "./Wallet"
 
-/** 
-  ~ What it does? ~
+/**
+ ~ What it does? ~
 
-  Displays an Address, Balance, and Wallet as one Account component,
-  also allows users to log in to existing accounts and log out
+ Displays an Address, Balance, and Wallet as one Account component,
+ also allows users to log in to existing accounts and log out
 
-  ~ How can I use? ~
+ ~ How can I use? ~
 
-  <Account
-    address={address}
-    localProvider={localProvider}
-    userProvider={userProvider}
-    mainnetProvider={mainnetProvider}
-    price={price}
-    web3Modal={web3Modal}
-    loadWeb3Modal={loadWeb3Modal}
-    logoutOfWeb3Modal={logoutOfWeb3Modal}
-    blockExplorer={blockExplorer}
-    isContract={boolean}
-  />
+ <Account
+ address={address}
+ localProvider={localProvider}
+ userProvider={userProvider}
+ mainnetProvider={mainnetProvider}
+ price={price}
+ web3Modal={web3Modal}
+ loadWeb3Modal={loadWeb3Modal}
+ logoutOfWeb3Modal={logoutOfWeb3Modal}
+ blockExplorer={blockExplorer}
+ isContract={boolean}
+ />
 
-  ~ Features ~
+ ~ Features ~
 
-  - Provide address={address} and get balance corresponding to the given address
-  - Provide localProvider={localProvider} to access balance on local network
-  - Provide userProvider={userProvider} to display a wallet
-  - Provide mainnetProvider={mainnetProvider} and your address will be replaced by ENS name
-              (ex. "0xa870" => "user.eth")
-  - Provide price={price} of ether and get your balance converted to dollars
-  - Provide web3Modal={web3Modal}, loadWeb3Modal={loadWeb3Modal}, logoutOfWeb3Modal={logoutOfWeb3Modal}
-              to be able to log in/log out to/from existing accounts
-  - Provide blockExplorer={blockExplorer}, click on address and get the link
-              (ex. by default "https://etherscan.io/" or for xdai "https://blockscout.com/poa/xdai/")
-**/
+ - Provide address={address} and get balance corresponding to the given address
+ - Provide localProvider={localProvider} to access balance on local network
+ - Provide userProvider={userProvider} to display a wallet
+ - Provide mainnetProvider={mainnetProvider} and your address will be replaced by ENS name
+ (ex. "0xa870" => "user.eth")
+ - Provide price={price} of ether and get your balance converted to dollars
+ - Provide web3Modal={web3Modal}, loadWeb3Modal={loadWeb3Modal}, logoutOfWeb3Modal={logoutOfWeb3Modal}
+ to be able to log in/log out to/from existing accounts
+ - Provide blockExplorer={blockExplorer}, click on address and get the link
+ (ex. by default "https://etherscan.io/" or for xdai "https://blockscout.com/poa/xdai/")
+ **/
 
 export default function Account({
     address,
@@ -92,11 +93,7 @@ export default function Account({
     return (
         <div style={{ display: "flex" }}>
             {display}
-            {web3Modal && (
-                <Button style={{ marginLeft: 8 }} shape="round" onClick={accountButtonInfo.action}>
-                    {accountButtonInfo.name}
-                </Button>
-            )}
+            {web3Modal && <ConnectButton moralisAuth={false} />}
         </div>
     )
 }
--- packages/react-app/src/views/Staking.jsx
+++ packages/react-app/src/views/Staking.jsx
@@ -14,15 +14,18 @@ function Staking({ yourLocalBalance, readContracts }) {
     // you can also use hooks locally in your component of choice
     // in this case, let's keep track of 'purpose' variable from our contract
     const purpose = useContractReader(readContracts, "YourContract", "purpose")
+    const [amountStaked, setAmountStaked] = useState(0)
 
     const handleStake = amount => {
         // Send the transaction to the blockchain to stake the specified amount
         console.log(`Staking ${amount} ETH...`)
+        setAmountStaked(amountStaked + amount)
     }
 
     const handleWithdraw = amount => {
         // Send the transaction to the blockchain to withdraw the specified amount
         console.log(`Withdrawing ${amount} ETH...`)
+        setAmountStaked(amountStaked - amount)
     }
 
     return (
@@ -35,10 +38,15 @@ function Staking({ yourLocalBalance, readContracts }) {
             }}
         >
             <h1>Staking App</h1>
+            <AmountStaked amount={amountStaked} />
             <StakingForm onStake={handleStake} />
-            <WithdrawForm onWithdraw={handleWithdraw} />
+            <WithdrawForm onWithdraw={handleWithdraw} amountStaked={amountStaked} />
         </div>
     )
+}
+
+const AmountStaked = ({ amount }) => {
+    return <div style={{ marginBottom: "16px" }}>You staked {amount} ETH.</div>
 }
 
 const StakingForm = ({ onStake }) => {
@@ -54,13 +62,20 @@ const StakingForm = ({ onStake }) => {
         }
     }
 
+    const handleInputChange = e => {
+        const regExp = /^[0-9.]*$/
+        if (regExp.test(e.target.value) || e.target.value === "") {
+            setAmount(e.target.value)
+        }
+    }
+
     return (
         <div style={{ display: "flex", alignItems: "center", marginBottom: "16px" }}>
             <Input
                 style={{ width: "200px" }}
                 placeholder="Enter amount to stake"
                 value={amount}
-                onChange={e => setAmount(e.target.value)}
+                onChange={handleInputChange}
             />
             <Button
                 style={{
@@ -78,39 +93,62 @@ const StakingForm = ({ onStake }) => {
     )
 }
 
-const WithdrawForm = ({ onWithdraw }) => {
-    const [amount, setAmount] = useState("")
+const WithdrawForm = ({ onWithdraw, amountStaked }) => {
+    const [amountToWithdraw, setAmountToWithdraw] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
 
     const handleWithdraw = () => {
         // Validate the amount before submitting the transaction
-        if (parseFloat(amount) > 0) {
-            onWithdraw(parseFloat(amount))
-            setAmount("")
+        const parsedWithdrawAmount = parseFloat(amountToWithdraw)
+        if (parsedWithdrawAmount > amountStaked) {
+            setErrorMessage("You can't withdraw more than you staked")
+        } else if (parsedWithdrawAmount > 0) {
+            onWithdraw(parsedWithdrawAmount)
+            setAmountToWithdraw("")
+            setErrorMessage("")
         } else {
-            alert("Please enter a valid amount")
+            setErrorMessage("Please enter a valid amount")
+        }
+    }
+
+    const handleInputChange = e => {
+        const regExp = /^[0-9.]*$/
+        if (regExp.test(e.target.value) || e.target.value === "") {
+            setAmountToWithdraw(e.target.value)
+            setErrorMessage("")
         }
     }
 
     return (
-        <div style={{ display: "flex", alignItems: "center", marginBottom: "16px" }}>
-            <Input
-                style={{ width: "200px" }}
-                placeholder="Enter amount to withdraw"
-                value={amount}
-                onChange={e => setAmount(e.target.value)}
-            />
-            <Button
-                style={{
-                    width: "100px",
-                    paddingLeft: "16px",
-                    paddingRight: "16px",
-                    marginLeft: "16px"
-                }}
-                type="primary"
-                onClick={handleWithdraw}
-            >
-                Withdraw
-            </Button>
+        <div style={{ marginBottom: "16px" }}>
+            <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                <Input
+                    style={{
+                        width: "200px",
+                        border: errorMessage ? "1px solid red" : ""
+                    }}
+                    placeholder="Enter amount to withdraw"
+                    value={amountToWithdraw}
+                    onChange={handleInputChange}
+                />
+                <Button
+                    style={{
+                        width: "100px",
+                        paddingLeft: "16px",
+                        paddingRight: "16px",
+                        marginLeft: "16px"
+                    }}
+                    type="primary"
+                    onClick={handleWithdraw}
+                >
+                    Withdraw
+                </Button>
+            </div>
+            {errorMessage && (
+                <div style={{ color: "red", fontSize: "12px", marginTop: "8px" }}>
+                    {errorMessage}
+                </div>
+            )}
         </div>
     )
 }
